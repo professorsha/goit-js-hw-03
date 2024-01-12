e38b42e5d0ac3ff0dcaddab7941b3f8c7848a67a
professorsha <69015177+professorsha@users.noreply.github.com>
2024-01-13 00:12:37 +0200

    final

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,12 @@
-// Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
-
-// Усередині функції:
-
-// Використай цикл для ітерації кожного елемента масиву numbers.
-// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
-// Поверни свій новий масив з підходящими числами як результат.
-function filterArray(numbers, value){
-let newArray=[];
-const n=numbers.length;
-for (let i=0;i<=n;i++){
-    if (numbers[i]>value){
-        newArray.push(numbers[i]);
+function filterArray(numbers, value) {
+  let newArray = [];
+  const n = numbers.length;
+  for (let i = 0; i <= n; i++) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
     }
-}
-return newArray;
+  }
+  return newArray;
 }
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
